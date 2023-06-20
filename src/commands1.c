f4ee156e9265a4b620515dc8dd256ce1586efb52
hoangnhatminh <minh.hn215426@sis.hust.edu.vn>
2023-06-20 21:04:09 +0700

    ???

--- src/commands1.c
+++ src/commands1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include "commands.h"
 
 void exitFunc(int argc, char **argv) {
@@ -7,11 +9,25 @@ void exitFunc(int argc, char **argv) {
 }
 
 void cdFunc(int argc, char **argv) {
-	printf("cd command\n");
+	char * temp;
+	temp = argv[1];
+	if(argc > 3) {
+		puts("\nError
+			  \nToo many arguments");
+	}
+
+	else if(temp == "NULL" || temp == "~")
+		chdir(home);
+	
+	else {
+		int change = chdir(temp);
+		if(change < 1)
+			printf("Directory does not exist :'(");
+	}
 }
 
 void dateFunc(int argc, char **argv) {
-	printf("date command\n");
+	int
 }
 
 void dirFunc(int argc, char **argv) {
