5f58a303cf0cd5b762fe9ae7a570d731463ec85f
hoangnhatminh0809 <92902370+hoangnhatminh0809@users.noreply.github.com>
2023-06-26 11:23:00 +0700

    Update commands1.c

--- src/commands1.c
+++ src/commands1.c
@@ -42,25 +42,18 @@ void cdFunc(int argc, char **argv){
 }
 
 void dateFunc(int argc, char **argv) {
-   time_t now = time(0);
+    time_t now = time(0);
+    struct tm *ltm = localtime(&now);
 
-   struct tm *ltm = localtime(&now);
+    int hour = ltm->tm_hour;
+    int min = ltm->tm_min;
+    int day = ltm->tm_mday;
+    int month = ltm->tm_mon + 1;
+    int year = 1900 + ltm->tm_year;
 
-   int year = 1900 + ltm->tm_year;
-   int month = ltm->tm_mon;
-   int day = ltm->tm_mday;
-   int hour = 1 + ltm->tm_hour;
-   int min = 1 + ltm->tm_min;
-   int sec = 1 + ltm->tm_sec;
+    const char *days[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 
-   // Mảng chứa tên các tháng
-   const char* months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-
-   printf("Year: %d\n", year);
-   printf("Month: %s\n", months[month]);
-   printf("Day: %d\n", day);
-   printf("Time: %d:%d:%d\n", hour, min, sec);
-
+    printf("%02d:%02d %s %02d/%02d/%d\n", hour, min, days[ltm->tm_wday], day, month, year);
 }
 
 void dirFunc(int argc, char **argv) {
