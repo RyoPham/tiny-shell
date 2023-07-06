cbf3b213bba1f553f6dd4f7e715dc607612403fc
RyoPham <minhsnow03@gmail.com>
2023-07-06 22:01:53 +0700

    Add apps for demo. Fix a process bug: SIGINT is sent to all process childs.

---
+++ app folder/infinities.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid = fork(), t;
+    int status;
+    if(pid == 0) {
+        pid = fork();
+        if(pid == 0) {
+            // while(1) {
+            //     t = getpid();
+            //     printf("Hello from %u\n", t);
+            //     sleep(1);
+            // }
+            while(1);
+        }
+        else {
+            // while(1) {
+            //     t = getpid();
+            //     printf("Hello from %u\n", t);
+            //     sleep(1);
+            // }
+            while(1);
+        }
+    }
+    else {
+        pid = fork();
+        if(pid == 0) {
+            while(1);
+        }
+        else {
+            while(1);
+        }
+    }
+}
